fe24596db42ad02d215688bdfc6ea77fc2343516
Steeve bernard <steeve@Steeves-MacBook-Pro.local>
2020-04-30 13:15:13 -0300

    dfd

---
+++ lib/screens/chats/chats-conversations.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/foundation.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class ChatConversationsScreen extends StatefulWidget {
+  final String title;
+  final WebSocketChannel channel;
+
+  ChatConversationsScreen({Key key, @required this.title, @required this.channel})
+      : super(key: key);
+
+  @override
+  _ChatConversationsScreenState createState() => _ChatConversationsScreenState();
+}
+
+class _ChatConversationsScreenState extends State<ChatConversationsScreen> {
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Form(
+              child: TextFormField(
+                controller: _controller,
+                decoration: InputDecoration(labelText: 'Send a message'),
+              ),
+            ),
+            StreamBuilder(
+              stream: widget.channel.stream,
+              builder: (context, snapshot) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sendMessage,
+        tooltip: 'Send message',
+        child: Icon(Icons.send),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      widget.channel.sink.add(_controller.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.channel.sink.close();
+    super.dispose();
+  }
+}
--- lib/screens/home-screen.dart
+++ lib/screens/home-screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:morse/screens/chats/chats-conversations.dart';
 import 'package:morse/screens/conversations-screen.dart';
 import 'package:morse/screens/settings-screen.dart';
+import 'package:web_socket_channel/io.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -57,7 +59,19 @@ class _HomeScreenState extends State<HomeScreen> {
               elevation: 7.0,
               child: new Icon(Icons.add_comment),
               backgroundColor: Theme.of(context).primaryColor,
-              onPressed: () {}),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => 
+                ChatConversationsScreen(
+                  title: 'Welcome dados',
+                  channel: IOWebSocketChannel.connect('ws://186.207.238.21:8006/ws/mensagem/steeve', 
+                    headers: {
+                      'Cookie': 'sessionid=8gw1off3j5p0puts6d1aa3kuvcw65a3i',
+                    }
+                  ),
+                ),
+                
+                ));
+              }),
           bottomNavigationBar: ClipRRect(
             borderRadius: BorderRadius.only(
               topRight: Radius.circular(40),
